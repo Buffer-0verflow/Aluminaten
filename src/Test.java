4dec7845cfd17542b683237104992f881084178c
DonTrash <buechelerkon@gmail.com>
2018-06-26 12:23:35 +0200

    test

--- src/Test.java
+++ src/Test.java
@@ -7,10 +7,6 @@ import java.util.List;
 public class Test {
 
 	public static void main(String[] args) {
-		/*KategorieManager manager = new KategorieManager();
-		for (Kategorie p : (ArrayList<Kategorie>) manager.listKategorie())
-			System.out.println(p);*/
-
 	
 	}
 	
